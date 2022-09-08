ec0ee1c449657538b81f58eaf3b89fd98354442e
Glsme <glasses.str.man@gmail.com>
2022-09-08 15:16:29 +0900

    Feat: BaseView, BaseViewController 추가

---
+++ BanGiDa/Utility/Base/BaseView.swift
@@ -0,0 +1,31 @@
+//
+//  BaseView.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 2022/09/08.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    func configureUI() {
+        
+    }
+    
+    func setConstraints() {
+        
+    }
+
+}
---
+++ BanGiDa/Utility/Base/BaseViewController.swift
@@ -0,0 +1,21 @@
+//
+//  BaseViewController.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 2022/09/08.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureUI()
+    }
+    
+    func configureUI() {
+        
+    }
+}
