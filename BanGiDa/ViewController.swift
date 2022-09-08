443b750e1df45660144203de67dbe91e3259d0e4
Glsme <glasses.str.man@gmail.com>
2022-09-08 14:58:20 +0900

    Initial Commit

---
+++ BanGiDa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 2022/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
