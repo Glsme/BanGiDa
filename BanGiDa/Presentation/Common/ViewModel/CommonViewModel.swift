7dd58fcab1b0e76992f33a208620b42ff1ae3884
Glsme <glasses.str.man@gmail.com>
2022-09-16 16:24:14 +0900

    Build: Common View Model 재업로드

---
+++ BanGiDa/Presentation/Common/ViewModel/CommonViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  CommonViewModel.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 2022/09/15.
+//
+
+import Foundation
+
+class CommonViewModel {
+    let selectButtonList = [
+        SelectButtonModel(title: "메모", imageString: "note.text", r: 133, g: 204, b: 204, alpha: 1, placeholder: "오늘 있었던 일을 적어주세요."),
+        SelectButtonModel(title: "알람", imageString: "alarm", r: 252, g: 200, b: 141, alpha: 1, placeholder: ""),
+        SelectButtonModel(title: "진료 기록", imageString: "cross.case", r: 169, g: 223, b: 225, alpha: 1, placeholder: "병원 진료를 적어주세요."),
+        SelectButtonModel(title: "목욕", imageString: "drop", r: 250, g: 227, b: 175, alpha: 1, placeholder: "오늘 목욕할 때 있었던 일을 적어주세요."),
+        SelectButtonModel(title: "약", imageString: "pills", r: 168, g: 215, b: 177, alpha: 1, placeholder: "약 복용 기록을 적어주세요."),
+        SelectButtonModel(title: "이상 증상", imageString: "bandage", r: 228, g: 167, b: 180, alpha: 1, placeholder: "이상 증상을 적어주세요.")
+    ]
+    
+    let memoType = ["memo", "alarm", "hospital", "shower", "pill", "abnormal"]
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.timeZone = TimeZone(identifier: "UTC")
+        formatter.dateFormat = "yyyy년 MM월 dd일 EEEE"
+        return formatter
+    }()
+}
+
+extension CommonViewModel {
+    
+    enum MemoType: String {
+        case memo = "memo"
+        case alarm = "alarm"
+        case hospital = "hospital"
+        case shower = "shower"
+        case pill = "pill"
+        case abnormal = "abnormal"
+    }
+}
