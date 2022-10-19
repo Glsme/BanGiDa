5f00c915cff5265509cce08e4df5337b1c185c51
Glsme <glasses.str.man@gmail.com>
2022-10-20 03:13:07 +0900

    Refactor: Setting UI 코드 정리

--- BanGiDa/Presentation/Setting/View/SettingViewController.swift
+++ BanGiDa/Presentation/Setting/View/SettingViewController.swift
@@ -179,6 +179,12 @@ extension SettingViewController: UICollectionViewDelegate {
             }
         }
     }
+    
+    private func moveToReview() {
+        if let reviewURL = URL(string: "itms-apps://itunes.apple.com/app/itunes-u/id\(6443524869)?ls=1&mt=8&action=write-review"), UIApplication.shared.canOpenURL(reviewURL) {
+            UIApplication.shared.open(reviewURL, options: [:], completionHandler: nil)
+        }
+    }
 }
 
 extension SettingViewController: UIDocumentPickerDelegate {
@@ -246,87 +252,6 @@ extension SettingViewController: UIDocumentPickerDelegate {
     }
 }
 
-extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        viewModel.setSectionHeaderView(section: section)
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.setCellNumber(section: section)
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.reuseIdentifier, for: indexPath) as? SettingTableViewCell else { return UITableViewCell() }
-        
-        if indexPath.section == 2, indexPath.row == 1 {
-            cell.backgroundColor = .memoBackgroundColor
-            cell.label.text = viewModel.setCellText(indexPath: indexPath)
-            cell.selectionStyle = .none
-            cell.versionLabel.text = version
-            cell.image.isHidden = true
-        } else {
-            cell.backgroundColor = .memoBackgroundColor
-            cell.label.text = viewModel.setCellText(indexPath: indexPath)
-            cell.selectionStyle = .none
-            cell.versionLabel.isHidden = true
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-        if indexPath.section == 0 {
-            switch indexPath.row {
-            case 0:
-                backupFileButtonClicked()
-            case 1:
-                restoreFileButtonClicked()
-            case 2:
-                showSelectAlert(message: "데이터 초기화 시 기존 데이터는 전부 사라집니다. \n\n데이터 초기화를 진행할까요?") { _ in
-                    self.viewModel.resetData()
-                    let walkthorughVC = WalkThroughViewController()
-                    self.tabBarController?.selectedIndex = 0
-                    self.transViewController(ViewController: walkthorughVC, type: .presentFullscreen)
-                }
-                break
-            default:
-                break
-            }
-        } else if indexPath.section == 1 {
-            if indexPath.row == 0 {
-                moveToReview()
-            } else if indexPath.row == 1 {
-                sendMail()
-            }
-        } else if indexPath.section == 2 {
-            if indexPath.row == 0 {
-                
-                guard let url = Bundle.main.url(forResource: "Package", withExtension: "resolved"),
-                      let data = try? Data(contentsOf: url),
-                      let acknowList = try? AcknowPackageDecoder().decode(from: data) else {
-                    return
-                }
-                
-                let vc = AcknowListViewController()
-                vc.acknowledgements = acknowList.acknowledgements
-                transViewController(ViewController: vc, type: .push)
-            }
-        }
-    }
-    
-    private func moveToReview() {
-        if let reviewURL = URL(string: "itms-apps://itunes.apple.com/app/itunes-u/id\(6443524869)?ls=1&mt=8&action=write-review"), UIApplication.shared.canOpenURL(reviewURL) {
-            UIApplication.shared.open(reviewURL, options: [:], completionHandler: nil)
-        }
-    }
-}
-
 extension SettingViewController : MFMailComposeViewControllerDelegate {
     
     private func sendMail() {
@@ -339,7 +264,6 @@ extension SettingViewController : MFMailComposeViewControllerDelegate {
             self.present(mail, animated: true)
             
         } else {
-            
             showAlert(message: "메일 등록을 해주시거나 glasses.str.man@gmail.com으로 문의주세요.")
         }
     }
