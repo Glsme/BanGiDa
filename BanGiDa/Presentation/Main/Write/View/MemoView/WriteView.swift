8821c8a2570fc5f43c8a8c724a4e052e7bb01b13
Glsme <glasses.str.man@gmail.com>
2023-07-23 17:11:19 +0900

    Feat: WriteView 생성

---
+++ BanGiDa/Presentation/Main/Write/View/MemoView/WriteView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteView.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 2023/07/23.
+//
+
+import SwiftUI
+
+struct WriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WriteView_Previews: PreviewProvider {
+    static var previews: some View {
+        WriteView()
+    }
+}
