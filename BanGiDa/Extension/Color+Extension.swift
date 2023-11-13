c27a8e4142ceed72b8e12ce5a6f476e386c93868
Glsme <glasses.str.man@gmail.com>
2023-11-13 18:17:05 +0900

    Feat: Color+Extension 생성 및 적용

---
+++ BanGiDa/Extension/Color+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Extension.swift
+//  BanGiDa
+//
+//  Created by Seokjune Hong on 11/13/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemTintColor = Color("SystemTintColor")
+}
