82d5fe765b6bf819a72ca07a449cee774626b4ea
Glsme <glasses.str.man@gmail.com>
2024-12-03 23:32:04 +0900

    Feat: SkyMint Color 추가

--- BanGiDa/Extension/UIColor+Extension.swift
+++ BanGiDa/Extension/UIColor+Extension.swift
@@ -9,6 +9,7 @@ import UIKit
 
 extension UIColor {
     static let bananaYellow = UIColor(red: 255/255, green: 248/255, blue: 195/255, alpha: 1)
+    static let skyMint = UIColor(red: 157/255, green: 208/255, blue: 221/255, alpha: 1)
     static let ultraLightGray = UIColor(red: 200/255, green: 200/255, blue: 200/255, alpha: 1)
     static let unaBlue = UIColor(red: 26/255, green: 198/255, blue: 255/255, alpha: 1)
     static let skyBlue = UIColor(red: 169/255, green: 223/255, blue: 225/255, alpha: 1)
